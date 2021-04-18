ed19dbd5495600716833bcebb39542bc9a94e7be
Matthew Mattox <mmattox@support.tools>
2021-04-18 00:16:01 -0500

    Adding go web server

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
-import "fmt"
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://opsdata.io, 301)
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Server", "OpsData.io")
+}
+
+
 func main() {
-    fmt.Println("hello world")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/healthz", healthz)
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
 }
