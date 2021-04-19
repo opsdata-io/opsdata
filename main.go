24245d2474b34c7d47f285b4da19df9a5e8b1a8f
Matthew Mattox <mmattox@support.tools>
2021-04-18 21:58:25 -0500

    Moving helm files around

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-    app := fiber.New()
+	dsn := "opsdata:opsdata@tcp(10.42.0.19:3306)/opsdata"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+	if err != nil {
+		panic("Could not connect to the database")
+	}
 
-    app.Listen(":8000")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":8000")
 }
