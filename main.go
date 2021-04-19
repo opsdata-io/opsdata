37aa5be6ea08eaaead2e041102e830a41310e5c5
Matthew Mattox <mmattox@support.tools>
2021-04-18 21:24:51 -0500

    Work in-progress

--- main.go
+++ main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"github.com/gofiber/fiber/v2"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://opsdata.io", 301)
-}
-
-func healthz(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Server", "OpsData.io")
-}
-
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/healthz", healthz)
-	log.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World 👋!")
+    })
+
+    app.Listen(":8000")
 }
