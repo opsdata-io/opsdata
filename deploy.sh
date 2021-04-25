2eea38d7b143a3a90c638d54111d256d1c046976
Matthew Mattox <mmattox@support.tools>
2021-04-25 00:35:51 -0500

    Adding UI

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 echo "customizing Deployment files..."
 mkdir ./deployment-ready/
-cd ./deployment
+cd ./k8s/deployment
 for file in `ls *.yaml`
 do
   echo "Working on $file"
   cat $file | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > ../deployment-ready/"$file"
 done
-cd ../
+cd ../../
 pwd
 
 echo "customizing helm values..."
