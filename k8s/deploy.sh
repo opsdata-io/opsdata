6966edfb4f2edc5c164359c00905d888a654aeeb
Matthew Mattox <mmattox@support.tools>
2021-04-25 00:53:54 -0500

    Fixing k8s deployment files

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+cd 	/drone/src/k8s/
 echo "customizing Deployment files..."
-mkdir ./deployment-ready/
-cd ./k8s/deployment
+mkdir -p ./deployment-ready/
+cd ./deployment
 for file in `ls *.yaml`
 do
   echo "Working on $file"
   cat $file | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > ../deployment-ready/"$file"
 done
-cd ../../
-pwd
 
+
+cd 	/drone/src/
 echo "customizing helm values..."
 mkdir -p ./helm-config-ready/mariadb-galera/
 cat ./helm-config/mariadb-galera/values.yaml | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > ./helm-config-ready/mariadb-galera/values.yaml
