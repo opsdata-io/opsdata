74aa01d8f4929bc31311ce63909b89ec32798bb9
Matthew Mattox <mmattox@support.tools>
2021-05-01 01:12:44 -0500

    Adding logging

--- api/database/connection.go
+++ api/database/connection.go
@@ -2,11 +2,14 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/mattmattox/opsdata/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -53,10 +56,21 @@ func Connect() {
 		databaseName = "opsdata"
 	}
 
-	//dsn := "opsdata:opsdata@tcp(localhost:3306)/opsdata"
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+
 	dsn := fmt.Sprint(databaseUsername, ":", databasePassword, "@tcp(", databaseHostname, ":", databasePort, ")/", databaseName, "?parseTime=true")
 
-	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 
 	if err != nil {
 		panic("Could not connect to the database")
--- api/main.go
+++ api/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/mattmattox/opsdata/database"
 	"github.com/mattmattox/opsdata/routes"
-	"gorm.io/gorm/logger"
 )
 
 func main() {
@@ -14,7 +12,6 @@ func main() {
 	database.Connect()
 
 	app := fiber.New()
-	app.Use(logger.New())
 
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
