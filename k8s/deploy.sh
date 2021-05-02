f7d0590711144a72ed955003ed64851bec7c0a5f
Matthew Mattox <mmattox@support.tools>
2021-05-02 02:24:24 -0500

    Testing user local

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -15,3 +15,9 @@ cd 	/drone/src/
 echo "customizing helm values..."
 mkdir -p ./helm-config-ready/mariadb-galera/
 cat ./helm-config/mariadb-galera/values.yaml | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > ./helm-config-ready/mariadb-galera/values.yaml
+
+cd 	/drone/src/
+echo "customizing master files..."
+REPO_USER=`echo ${DRONE_REPO} | awk -F'/' '{print $1}'`
+mkdir -p ./ingress-ready/
+cat ./ingress/master.yaml | sed "s/REPO_USER/${REPO_USER}/g" > ./ingress-ready/master.yaml
