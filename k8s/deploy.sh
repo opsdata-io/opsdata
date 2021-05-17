f7aaee4b1e0eb6a2c3dc0ce39194052e1feb8f51
Matthew Mattox <matt.mattox@suse.com>
2021-05-17 01:46:18 -0500

    Fixing missing ingress for master

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -16,8 +16,10 @@ echo "customizing helm values..."
 mkdir -p ./helm-config-ready/mariadb-galera/
 cat ./helm-config/mariadb-galera/values.yaml | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > ./helm-config-ready/mariadb-galera/values.yaml
 
-cd 	/drone/src/
+cd 	/drone/src/k8s/
 echo "customizing master files..."
 REPO_USER=`echo ${DRONE_REPO} | awk -F'/' '{print $1}'`
+echo "Drone Repo: ${DRONE_REPO}"
+echo "Repo User: ${REPO_USER}"
 mkdir -p ./ingress-ready/
 cat ./ingress/master.yaml | sed "s/REPO_USER/${REPO_USER}/g" > ./ingress-ready/master.yaml
