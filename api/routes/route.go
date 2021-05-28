87685db8761176a60de22605a24a52dbf2fcc1cb
Matthew Mattox <matt.mattox@suse.com>
2021-05-28 11:03:26 -0500

    Fixing metrics port

--- api/routes/route.go
+++ api/routes/route.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Setup(app *fiber.App) {
-	prometheus := fiberprometheus.New("my-service-name")
+	prometheus := fiberprometheus.New("opsdata-api")
 	prometheus.RegisterAt(app, "/metrics")
 	app.Use(prometheus.Middleware)
 	app.Get("/api", controllers.Homepage)
