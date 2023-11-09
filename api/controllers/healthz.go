5adbb3378115c68bce9ae3ced2c6394336726024
Matthew Mattox <mmattox@support.tools>
2023-11-09 17:20:49 -0600

    Cleaning up

--- api/controllers/healthz.go
+++
@@ -1,7 +0,0 @@
-package controllers
-
-import "github.com/gofiber/fiber/v2"
-
-func Healthz(c *fiber.Ctx) error {
-	return c.SendString("ok")
-}
--- api/controllers/homepage.go
+++
@@ -1,9 +0,0 @@
-package controllers
-
-import (
-	"github.com/gofiber/fiber/v2"
-)
-
-func Homepage(c *fiber.Ctx) error {
-	return c.Redirect("/")
-}
