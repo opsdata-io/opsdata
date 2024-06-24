49f29088dfcc8d17a20d203d81e4048d3945db2a
Matthew Mattox <mmattox@support.tools>
2024-06-24 18:31:33 -0500

    Adding debug

--- ui/src/utils/api.js
+++ ui/src/utils/api.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 // Get API_URL from environment variable or default to localhost
 export const API_URL = process.env.REACT_APP_BACKEND_API || 'http://localhost:8080';
 
+// Log the API_URL to verify it
+console.log("API_URL:", API_URL);
+
 export const login = async (credentials) => {
     return await axios.post(`${API_URL}/login`, credentials);
 };
