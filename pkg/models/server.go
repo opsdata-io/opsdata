f108e4ab7ef9565704d8bdb42a6c104076ddff05
Matthew Mattox <mmattox@support.tools>
2024-07-01 19:31:51 -0500

    [FIX] Fix struct tag syntax in Server model
    Correct the struct tags for the LastPing field in the Server struct to follow the correct syntax and functionality. This includes removing extraneous spaces, enclosing the tags in backticks, and setting the default value for LastPing to the current timestamp using the appropriate GORM tag.

--- pkg/models/server.go
+++ pkg/models/server.go
@@ -4,11 +4,11 @@ import "time"
 
 // Server represents a server in the database
 type Server struct {
-	ID          uint      `json:"id" gorm:"primaryKey; autoIncrement"`
+	ID          uint      `json:"id" gorm:"primaryKey;autoIncrement"`
 	CustomerID  uint      `json:"customerId" gorm:"index"` // Foreign key to Customer
 	Name        string    `json:"name" gorm:"not null"`
 	DeviceType  string    `json:"deviceType"`
 	IPAddress   string    `json:"ipAddress"`
 	Description string    `json:"description"`
-	LastPing    time.Time `json:"lastPing" gorm: "current_timestamp"`
+	LastPing    time.Time `json:"lastPing" gorm:"default:current_timestamp"`
 }
